c4daceb43724840e00774f09cfa1867b7c3e10f6
r1d3rzz <myintthwaykhine127@gmail.com>
2024-01-01 10:22:28 +0630

    create caculator with input validation

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
-class Animal {
-  constructor(name) {
-    this.name = name;
+const btn = document.getElementById("btn");
+let isError = false;
+
+btn.addEventListener("click", function () {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  let operator = document.getElementById("operator").value;
+  let res = null;
+
+  if (!validate(num1, num2)) {
+    [num1, num2] = changeInt([num1, num2]);
+
+    switch (operator) {
+      case "+":
+        res = num1 + num2;
+        break;
+      case "-":
+        res = num1 - num2;
+        break;
+      case "*":
+        res = num1 * num2;
+        break;
+      case "/":
+        res = num1 / num2;
+        break;
+      default:
+        res = 0;
+        break;
+    }
   }
 
-  eat() {
-    return `${this.name} is eating`;
+  if (res) {
+    document.getElementById("result").innerText = res;
+  } else {
+    document.getElementById("result").innerText = 0;
   }
+});
 
-  sleep() {
-    return `${this.name} is sleep`;
-  }
+function changeInt(strVals = []) {
+  return strVals.map((strVal) => parseInt(strVal));
 }
 
-class Dog extends Animal {
-  info() {
-    return `${this.name} is a dangerous dog.`;
+function validate(num1, num2) {
+  if (num1 == "" || num1 == undefined || num1 == null) {
+    document.getElementById("num1Error").textContent = "Num1 is required";
+    isError = true;
+  } else {
+    document.getElementById("num1Error").textContent = null;
   }
+
+  if (num2 == "" || num2 == undefined || num2 == null) {
+    document.getElementById("num2Error").textContent = "Num2 is required";
+    isError = true;
+  } else {
+    document.getElementById("num2Error").textContent = null;
+  }
+
+  if (isError) false;
+
+  return (isError = false);
 }
-
-const aungnet = new Dog("Aung Net");
-
-console.log(aungnet);
